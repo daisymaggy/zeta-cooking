26c6af97211e8b87b73e18fcbe6dcec6829af58a
Maggy Zhou <maggy.zhou@student-cs.fr>
2026-02-03 09:52:17 +0100

    fix: fix warning, middleware not applying for pre-rendered pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,14 @@
 import { defineMiddleware } from 'astro/middleware'
 
 export const onRequest = defineMiddleware((context, next) => {
-  const token = context.cookies.get('auth_token')?.value
+  if (!context.isPrerendered) {
+    const token = context.cookies.get('auth_token')?.value
 
-  const protectedRoutes = ['/favourites', '/profile']
+    const protectedRoutes = ['/favourites', '/profile']
 
-  if (protectedRoutes.some((path) => context.url.pathname.startsWith(path)) && !token) {
-    return Response.redirect(new URL('/login', context.url), 302)
+    if (protectedRoutes.some((path) => context.url.pathname.startsWith(path)) && !token) {
+      return Response.redirect(new URL('/login', context.url), 302)
+    }
   }
 
   return next()
