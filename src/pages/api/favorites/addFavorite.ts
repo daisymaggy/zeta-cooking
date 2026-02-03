9c63bd74a3b59d01d328ca9b2c4e861531a65f78
Fatha Mohamad <mohamad.fatha@student-cs.fr>
2026-02-03 14:48:34 +0100

    feat: create favorites server routes

---
+++ src/pages/api/favorites/addFavorite.ts
@@ -0,0 +1,61 @@
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request, cookies }) => {
+    try {
+        const token = cookies.get("auth_token")?.value;
+
+        if (!token) {
+            return new Response(JSON.stringify({ error: "Not authenticated" }), {
+                status: 401,
+                headers: { "Content-Type": "application/json" },
+            });
+        }
+
+        const { username, id } = await request.json();
+
+        if (!username || !id) {
+            return new Response(JSON.stringify({ error: "Username ou ID manquant" }), {
+                status: 400,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+
+        const res = await fetch(
+            `https://gourmet.cours.quimerch.com/users/${username}/favorites?recipeID=${id}`,
+            {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        );
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            return new Response(JSON.stringify({ error: data.message || "Echec de l'ajout de la recette aux favoris" }), {
+                status: res.status,
+                headers: { "Content-Type": "application/json" }
+            });
+        }
+
+
+        return new Response(
+            JSON.stringify(data),
+            {
+                status: 200,
+                headers: { "Content-Type": "application/json" }
+            }
+        );
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Erreur serveur" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" }
+        });
+    }
+}
