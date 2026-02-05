26740277a695a63746385665fe7508ea8faf4d59
Maggy Zhou <maggy.zhou@student-cs.fr>
2026-02-05 11:19:43 +0100

    fix: not use Astro.url

---
+++ src/lib/recipes.ts
@@ -0,0 +1,27 @@
+import type { Recipe } from '../types/recipe'
+
+const API_BASE = 'https://gourmet.cours.quimerch.com'
+
+export async function getRecipeById(id: string): Promise<Recipe> {
+  const res = await fetch(`${API_BASE}/recipes/${id}`, {
+    headers: { Accept: 'application/json' },
+  })
+
+  if (!res.ok) {
+    throw new Error('Recipe not found')
+  }
+
+  return res.json()
+}
+
+export async function getRelatedRecipes(id: string): Promise<Recipe[]> {
+  const res = await fetch(`${API_BASE}/recipes/${id}/related`, {
+    headers: { Accept: 'application/json' },
+  })
+
+  if (!res.ok) {
+    return []
+  }
+
+  return res.json()
+}
